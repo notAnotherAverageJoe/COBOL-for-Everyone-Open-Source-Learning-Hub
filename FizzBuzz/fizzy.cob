f421c4710f4d623820a0060685bc29e55c8b9616
joseph skokan <joeskokan20@gmail.com>
2024-08-07 22:31:35 -0400

    adding fizzbuzz

---
+++ FizzBuzz/fizzy.cob
@@ -0,0 +1,9 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. FizzBuzzProgram.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  fizz_total PIC 9(4).
+
+       PROCEDURE DIVISION.
+           DISPLAY "Enter the total number for FizzBuzz: "
