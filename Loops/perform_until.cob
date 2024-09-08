bb4303aeeb8f37aa6cc804ce252b9e90b949711a
joseph skokan <joeskokan20@gmail.com>
2024-09-07 23:40:31 -0400

    perform until

---
+++ Loops/perform_until.cob
@@ -0,0 +1,23 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. UNTIL_LOOP.
+      *PROGRAMMER  Joseph Skokan.
+      *COMPLETION DATE AUGUST 10, 2024.
+      *REMARKS.    THIS IS AN INTERACTIVE PROGRAM THAT IS DESIGNED
+      *            TO ILLUSTRATE THE USE OF COBOL.
+      *            THIS PROGRAM IS A SIMPLE EXAMPLE OF A 
+      *            PERFORM UNTIL LOOP.
+      *        compile with cobc -x -o until perform_until.cob 
+       
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       
+       01  NOT_YET_40  PIC 99 VALUE 0.
+       
+       PROCEDURE DIVISION.
+           DISPLAY "COUNTING UNTIL NOT YET 40 IS NOT LONG TRUE!".
+           PERFORM UNTIL NOT_YET_40 > 39
+               ADD 1 TO NOT_YET_40
+               DISPLAY "COUNT: " NOT_YET_40
+           END-PERFORM.
+           STOP RUN.
+       
